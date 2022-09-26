8f52b4bbd77d197570c6ceb78b53e00291d82ee5
Nick Nguyen <s2081459@tamu.edu>
2022-09-26 15:29:06 -0500

    Add random number function

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import discord
+import random
 from discord.ext import commands
 from googlesearch import search
 from dotenv import load_dotenv
@@ -26,11 +27,25 @@ async def lookup(ctx, *query):
     async with ctx.typing():
         for j in search(queryWhole, tld="co.in", num=1, stop=1, pause=2): 
             await ctx.send(f"\n:arrow_right: {j}")
+            return
 
 @bot.command()
 @commands.is_owner()
 async def shutdown(ctx):
     await ctx.channel.send(f"Shutting down!")
     exit()
+
+@bot.command()
+async def randnum(ctx, range1=int(0), range2=int(0)):
+    if (range2 != 0 and range2 <= range1):
+        await ctx.channel.send(f"Input a valid range!")
+        return
+    
+    if (range1 == 0 and range2 == 0):
+        await ctx.channel.send(f"{random.randint[1,10]}!")
+        return
+
+    await ctx.channel.send(f"{random.randint[range1, range2]}!")
+    return
 
 bot.run(TOKEN)
