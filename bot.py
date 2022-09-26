b0276d48516a5519cdc16e5602f26e36d48ec8e6
Nick Nguyen <s2081459@tamu.edu>
2022-09-26 14:57:27 -0500

    Add search function

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 import discord
 from discord.ext import commands
+from googlesearch import search
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,5 +17,20 @@ bot = commands.Bot(command_prefix='$', intents = intent)
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command()
+async def lookup(ctx, *query):
+    queryWhole=' '.join(query)
+    author = ctx.author.mention
+    await ctx.channel.send(f"Here is the top google search for {queryWhole} {author} !")
+    async with ctx.typing():
+        for j in search(queryWhole, tld="co.in", num=1, stop=1, pause=2): 
+            await ctx.send(f"\n:arrow_right: {j}")
+
+@bot.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.channel.send(f"Shutting down!")
+    exit()
 
 bot.run(TOKEN)
