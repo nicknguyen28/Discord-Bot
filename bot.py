e33c5b45186c41c511e9ffac72e19fc50d3b8747
Nick Nguyen <s2081459@tamu.edu>
2022-10-03 13:57:00 -0500

    Change name of instance.py to commands.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import asyncio
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -13,12 +14,14 @@ intent.members = True
 intent.message_content = True
 bot = commands.Bot(command_prefix = '$', intents = intent)
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+async def load():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
             await bot.load_extension(f'cogs.{filename[:-3]}')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command()
 async def reload(ctx):
@@ -26,13 +29,17 @@ async def reload(ctx):
         if filename.endswith('.py'):
             await bot.reload_extension(f'cogs.{filename[:-3]}')
     embed = discord.Embed(description = '**Reloaded!**')
-    await ctx.send(embed=embed)
+    await ctx.send(embed = embed)
 
 @bot.command()
 @commands.is_owner()
 async def shutdown(ctx):
     embed = discord.Embed(description = '**Shutdown!**')
-    await ctx.send(embed=embed)
+    await ctx.send(embed = embed)
     exit()
 
-bot.run(TOKEN)
+async def main():
+    await load()
+    await bot.start(TOKEN)
+
+asyncio.run(main())
